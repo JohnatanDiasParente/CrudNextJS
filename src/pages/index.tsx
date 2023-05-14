58e2b137caf8a38900581bde0426d9e574b5df59
John <79208491+Nefts@users.noreply.github.com>
2023-05-14 19:09:24 -0300

    #

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,38 @@
-import Head from 'next/head'
-import type {NextPage} from 'next'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.scss'
+import Head from "next/head";
+import type { NextPage } from "next";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.scss";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
       <main className={styles.container}>
         <form>
-          <input type='text' placeholder='Nome'></input>
-          <input type='email' placeholder='Email'></input>
-          <input type='tel' placeholder='Telefone'></input>
-          <textarea  placeholder='Observações'></textarea>
-          <button type="submit">salvar</button>
+          <input type="text" placeholder="Nome"></input>
+          <input type="email" placeholder="Email"></input>
+          <input type="tel" placeholder="Telefone"></input>
+          <textarea placeholder="Observações"></textarea>
+          <button type="submit">Salvar</button>
         </form>
-        <div >
-            <input type="text" placeholder='Buscar' />
+        <div className={styles.box}>
+          <input type="text" placeholder="Buscar" />
+          <div className={styles.contacts}>
+            <div className={styles.boxtitle}>
+              <p className={styles.nome}>NomeDaPessoa</p>
+              <div>
+                <a href="">Edit</a>
+                <a href="">Delete</a>
+              </div>
+            </div>
+            <div className={styles.info}>
+              <p>EmailDaPessoa</p>
+              <p>TelefoneDaPessoa</p>
+              <p>Descrição</p>
+            </div>
+          </div>
         </div>
       </main>
     </>
-  )
+  );
 }
