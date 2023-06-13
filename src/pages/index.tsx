cb46d2fdb2ef02c2060b75934046d6dda892da5a
John <79208491+Nefts@users.noreply.github.com>
2023-06-12 21:19:53 -0300

    1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,16 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.scss";
 
 import { database } from "../../services/firebase";
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
+
+type Contato ={
+  chave: string,
+  nome: string,
+  email: string,
+  telefone: string,
+  descricao: string,
+} //Criando uma tipagem para especificar o tipo de dado que vai ser enviado 
+
 
 const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
@@ -13,7 +22,26 @@ export default function Home() {
   const [telefone, setTelefone] = useState("");
   const [descricao, setDescricao] = useState("");
 
-  console.log(nome, email, telefone, descricao);
+  const[contatos, setContatos] = useState<Contato[]>()
+
+  useEffect(() => {
+    const refContatos = database.ref("contatos");
+    
+    refContatos.on("value", (resultado) => {
+      const resultadoContatos = Object.entries<Contato>(resultado.val() ?? {}).map(
+        ([chave, valor]) => {
+          return {
+            chave: chave,
+            nome: valor.nome,
+            email: valor.email,
+            telefone: valor.telefone,
+            descricao: valor.descricao,
+          };
+        });
+
+        setContatos(resultadoContatos)
+    });
+  }, []);
 
   function gravar(event: FormEvent) {
     event.preventDefault(); //Previne que a página seja recarregada
@@ -28,6 +56,11 @@ export default function Home() {
     };
 
     ref.push(dados); //Insere no banco de dados as informações do formulário
+
+    setNome("");
+    setEmail("");
+    setTelefone("");
+    setDescricao("");
   }
 
   return (
@@ -38,27 +71,31 @@ export default function Home() {
             type="text"
             placeholder="Nome"
             onChange={(event) => setNome(event.target.value)}
+            value={nome}
           ></input>
           {/*onChange usando o event com o useState para capturar os dados */}
           <input
             type="email"
             placeholder="Email"
             onChange={(event) => setEmail(event.target.value)}
+            value={email}
           ></input>
           <input
             type="tel"
             placeholder="Telefone"
             onChange={(event) => setTelefone(event.target.value)}
+            value={telefone}
           ></input>
           <textarea
             placeholder="Observações"
             onChange={(event) => setDescricao(event.target.value)}
+            value={descricao}
           ></textarea>
           <button type="submit">Salvar</button>
           {/* <input type="submit" value='salvar' /> funciona utilizando tanto button quanto submit*/}
         </form>
         <div className={styles.box}>
-          <input type="text" placeholder="Buscar" />
+          <input type="text" placeholder=" Buscar" />
           <div className={styles.contacts}>
             <div className={styles.boxtitle}>
               <p className={styles.nome}>Nome Da Pessoa</p>
